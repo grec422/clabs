9017c4c6400d430e6f53432b7053dbefc873b257
grec422 <68768620@qq.com>
2019-12-13 00:36:38 -0800

    added a ex1.c file

--- lab5/ex1.c
+++ lab5/ex1.c
@@ -1,37 +1,18 @@
 #include <stdio.h>
-#include <string.h>
-
-void reverse(char s[], int a,int b)
-{
-    int m;int n;
-    n=b-(a+1);
-    if(a<n)
-    {
-        m=s[a];
-        s[a]=s[n];
-        s[n]=m;
-        reverse(s,++a,b);
-    }
-}
-
 int main()
-{
-    char s;
-    char p[100];
-    int a=0;
-    int q=0;
-    while((s=getchar())!='\n')
-    {
-        p[a]=s;
-        q=++a;
-    }
-    reverse(p,0,q);
-    for(a=0;a<q;a++)
-    printf("%c",p[a]);
+{  int len,i;
+char str[100];
+
+gets(str);
+len=strlen(str);
+
+printf("outout:\n");
+
+for(i=len-1;i>=0;i--)
+printf("%c",str[i]);
+printf("\n");
+return 0;
 }
-
-
-
 
 
 
