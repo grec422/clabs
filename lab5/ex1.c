2c52c85fd856a41206c508ddec5817e57583978d
grec422 <68768620@qq.com>
2019-12-18 06:49:33 -0800

    added a ex1.c file

--- lab5/ex1.c
+++ lab5/ex1.c
@@ -1,19 +1,35 @@
-#include <stdio.h>
-int main()
-{  int len,i;
-char str[100];
-
-gets(str);
-len=strlen(str);
-
-printf("output:\n");
-
-for(i=len-1;i>=0;i--)
-printf("%c",str[i]);
-printf("\n");
-return 0;
+#include<stdio.h>
+#include<stdlib.h>
+int str(char *a)   
+{
+    if(*a=='\0')
+        return 0;
+    else
+        return str(++a)+1;
 }
-
+int reverse_string(char * string)
+{
+    int i=str(string);  
+    if(i<=1)      
+        ;
+    else
+    {
+    int m= string[0];    
+    string[0]=string[i-1]; 
+    string[i-1]='\0';      
+    reverse_string( string+1);
+    string[i-1]=m;       
+    }
+}
+int main()
+{ 
+	char a[100];
+     gets(a);
+    reverse_string(a);
+    printf("%s\n",a);
+    system("pause");
+    return 0;
+}
 
 
 
