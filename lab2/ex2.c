43c1d92a732002ebc6000d97d4ca827ee5e29f4e
grec422 <68768620@qq.com>
2019-12-25 07:44:07 -0800

    added a ex2.c file

--- lab2/ex2.c
+++ lab2/ex2.c
@@ -1,10 +1,23 @@
 #include<stdio.h>
-int num(int n){
+int num(int m)
+{
     int count=0;
-    while(n){
+    while(m)
+    {
         ++count;
-        n=n-1;
+        m=(m-1)&m;
     }
     return count;
 }
-int main()
+int main(int argc,char *argv[])
+{
+    char a;
+    int b=0;
+    while((a=getchar())!='\n')
+    {
+        b=b*10+a-'0';
+    }
+    printf("%d\n",num(b));
+    return 0;
+}
+
